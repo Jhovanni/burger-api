7d8b62853de9a427c4ac21b15561f9bdd1073680
jhovanni <ejhova@gmail.com>
2022-06-13 21:41:35 -0500

    Initial commit

---
+++ src/main/kotlin/com/jhovanni/burgerapi/BurgerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.jhovanni.burgerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BurgerApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BurgerApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/jhovanni/burgerapi/BurgerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jhovanni.burgerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BurgerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

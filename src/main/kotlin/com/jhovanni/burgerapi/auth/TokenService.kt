748750cb218013e3e8c6174c4f855a1589879b35
jhovanni <ejhova@gmail.com>
2022-06-14 07:43:35 -0500

    Move JWT key secret to property file

--- src/main/kotlin/com/jhovanni/burgerapi/auth/TokenService.kt
+++ src/main/kotlin/com/jhovanni/burgerapi/auth/TokenService.kt
@@ -2,20 +2,22 @@ package com.jhovanni.burgerapi.auth
 
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class TokenService {
+    @Value("security.jwt.secret")
+    private lateinit var secret: String
     fun generate(subject: String, roles: List<String>): String {
-        val secretKey = "javainuse"
         return Jwts
             .builder()
             .claim("authorities", roles)
             .setSubject(subject)
             .setIssuedAt(Date(System.currentTimeMillis()))
             .setExpiration(Date(System.currentTimeMillis() + 600000))
-            .signWith(SignatureAlgorithm.HS512, secretKey)
+            .signWith(SignatureAlgorithm.HS512, secret)
             .compact()
     }
 }
